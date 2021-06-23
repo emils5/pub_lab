953d1b90906ae04f7ec68935b9019f83b5cd0057
David Macara Brown <davidmacarabrown@gmail.com>
2021-06-23 11:39:04 +0100

    set up classes of pub, drink and customer

--- src/pub.py
+++ src/pub.py
@@ -1,5 +1,7 @@
 class Pub:
-    def __init__(self, name, cash):
+    def __init__(self, name, till):
         self.name = name
-        self.cash = cash
+        self.till = till
+        self.drinks = []
+
 
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -1,11 +1,18 @@
 import unittest
 
 from src.pub import *
+from src.customer import *
+from src.drink import *
 
 class TestPub(unittest.TestCase):
     
     def setUp(self):
         self.pub = Pub("The Prancing Pony", 100.00)
-    
+        self.customer = Customer("Frodo", 15.00)
+        self.customer = Customer("Samwise", 10.00)
+
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.name)
+
+    
+    
