644fc35b0dca577605680df62cf1563180117b6a
David Macara Brown <davidmacarabrown@gmail.com>
2021-06-23 10:41:11 +0100

    test files set up

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -3,4 +3,9 @@ import unittest
 from src.pub import *
 
 class TestPub(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.pub = Pub("The Prancing Pony", 100.00)
+    
+    def test_pub_has_name(self):
+        self.assertEqual("The Prancing Pony", self.pub.name)
