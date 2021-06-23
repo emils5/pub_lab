aa711140c1aaa1127472412950772a0d0db5ded5
David Macara Brown <davidmacarabrown@gmail.com>
2021-06-23 12:28:14 +0100

    add customer to pub

--- src/pub.py
+++ src/pub.py
@@ -3,9 +3,14 @@ class Pub:
         self.name = name
         self.till = till
         self.drinks = []
+        self.customers = []
     
     def add_drink_to_pub(self, drink):
         self.drinks.append(drink)
+    
+    def add_customer_to_pub(self, customer_to_add):
+        self.customers.append(customer_to_add)
+
 
 
 
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -8,8 +8,8 @@ class TestPub(unittest.TestCase):
     
     def setUp(self):
         self.pub = Pub("The Prancing Pony", 100.00)
-        self.customer = Customer("Frodo", 15.00)
-        self.customer = Customer("Samwise", 10.00)
+        self.customer1 = Customer("Frodo", 15.00)
+        self.customer2 = Customer("Samwise", 10.00)
 
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.name)
@@ -21,15 +21,29 @@ class TestPub(unittest.TestCase):
         drink = Drink("Beer", 3.50)
         self.pub.add_drink_to_pub(drink)
         self.assertEqual(1, len(self.pub.drinks))
+    
+    def test_add_customer_to_pub(self):
+        self.pub.add_customer_to_pub(self.customer1)
+        self.assertEqual(1, len(self.pub.customers))
+
+
+
+    # def test_find_customer_by_name(self):
+        
 
     
 
-
+# add customer to the pub [list]
+# find customer by name [for loop]
+# find number of customers in the pub len()
 
 # test pub till
 # test pub drinks
 # test customer by name
 # test customer's wallet
+
+# test add money to till
+# test remove money from customer
 
 
     
